1528df596c4ad8cb07af16b8ae6e9c7c9493001a
juanmont <juanmont@ucm.es>
2016-05-13 12:44:54 +0200

    el jugador gira con el raton

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,7 +7,8 @@ public class player : MonoBehaviour {
 	private Rigidbody2D body;
 	public float speed = 100;
 	public Bounds bounds;
-	
+
+    private Vector2 vDir;
 	
 	 void Awake()
 	 {
@@ -23,17 +24,21 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		float xAxis = Input.GetAxis("Horizontal");
-	        float yAxis = Input.GetAxis("Vertical");
-	
-	        Vector3 pos = transform.position;
-	        pos.x += xAxis * speed * Time.deltaTime;
-	        pos.y += yAxis * speed * Time.deltaTime;
+	    float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+	     pos.x += xAxis * speed * Time.deltaTime;
+	     pos.y += yAxis * speed * Time.deltaTime;
 	
 	       
-	        transform.position = pos;
-
-	}
+	     transform.position = pos;
+        // Rotate player
+        var objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - objectPos;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+    }
 
 
     void OnTriggerEnter2D(Collider2D other){
